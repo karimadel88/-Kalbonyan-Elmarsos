94f0d6852422e6e98c68929e952efc66f15b892c
Karim <karim@example.com>
2021-10-28 14:50:14 +0200

    update todos app

---
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch7- JavaSreipt on the Browser/todos-app 53 (challenge 3)/todos-app.js
@@ -0,0 +1,49 @@
+/**
+ * Listen for new todo creation
+ * 
+ * 
+ */
+
+let todos = [{
+    text : 'Read a book',
+    completed : false
+},{
+    text : 'Study two hours',
+    completed : true
+},{
+    text : 'Play a football match',
+    completed : true
+},{
+    text : 'Buy food',
+    completed : false
+},{
+    text : 'Order cat food',
+    completed : true
+}
+]
+
+// filter incomplated
+const incomplatedTodos = todos.filter(function(todo){
+    return !todo.completed
+})
+
+// add text to show length of incomplated todos
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incomplatedTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+const todosP = document.createElement('p')
+
+// add to html file n of incomplated todos by pragraoh element
+todos.forEach(function(todo){
+    const p = document.createElement('p')
+    if(!todo.completed){
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+    }
+})
+
+
+// Listen for new todo creation
+document.querySelector('button').addEventListener('click',function(e){
+    console.log('Add a new todo ...')
+})
