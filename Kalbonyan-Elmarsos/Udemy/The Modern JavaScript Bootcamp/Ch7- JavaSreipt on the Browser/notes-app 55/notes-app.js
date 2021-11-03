c3d3b4ea73c9ed4f4671f23efbbf3223c542b52a
Karim <karim@example.com>
2021-11-04 00:10:40 +0200

    update

---
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch7- JavaSreipt on the Browser/notes-app 55/notes-app.js
@@ -0,0 +1,34 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to italy'
+},{
+    title: 'read a book',
+    body: 'I would like to read a novel'
+},{
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better'
+}
+]
+console.log('fff')
+
+document.querySelector('#create-note').addEventListener('click',function(event){
+    console.log('Did this work?')
+    event.target.textContent ='the button was clicked' // from structure of event
+})
+
+document.querySelector('#remove-all').addEventListener('click',function(){
+    console.log('Delete all notes')
+    document.querySelectorAll('.note').forEach(function(note){
+        note.remove()
+    })
+})
+
+document.querySelector('#search-text').addEventListener('change',function(e){
+    console.log(e)
+})
+
+
+
+// document.querySelectorAll('button')[1].addEventListener('click',function(){
+//     console.log('Delete all notes')
+// })
