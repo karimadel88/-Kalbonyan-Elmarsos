aa1f29666a783e79ac42f9d994c5fdd7013efece
Karim <karim@example.com>
2021-11-11 16:31:43 +0200

    Refacroing notes app

---
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch8- Data Storage, Libraries, and More/notes-app 64/notes-app.js
@@ -0,0 +1,24 @@
+const notes = getSavedNotes()
+
+const filters = {
+    searchText: ''
+}
+    
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    notes.push({
+        title:'',
+        body:''
+    })
+    sitNotes('notes',notes)
+    renderNotes(notes,filters)
+})
+    
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+    
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
+
