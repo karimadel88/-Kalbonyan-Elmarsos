6d08e1eae32fb739ee7e979ff91b9231f0aeea5f
Karim <karim@example.com>
2021-11-11 11:32:23 +0200

    Relod and storge item

--- Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch8- Data Storage, Libraries, and More/notes-app 63/notes-app.js
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch8- Data Storage, Libraries, and More/notes-app 63/notes-app.js
@@ -1,35 +1,52 @@
+let notes = []
 
-const notes = []
+const filters = {
+    searchText: ''
+}
 
-    const filters = {
-        searchText: ''
-    }
+// Checked for existing saved data
+const notesJSON = localStorage.getItem('notes')
 
-    const renderNotes = function (notes, filters) {
-        const filteredNotes = notes.filter(function (note) {
-            return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-        })
+if(notesJSON !== null){
+    notes = JSON.parse(notesJSON)
+
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
     
-        document.querySelector('#notes').innerHTML = ''
-        
-        filteredNotes.forEach(function (note) {
-            const noteEl = document.createElement('p')
+document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+
+        if(note.title.length > 0){
             noteEl.textContent = note.title
-            document.querySelector('#notes').appendChild(noteEl)
-        })
-    }
+        }else{
+            noteEl.textContent = 'unnamed'
+        }
+
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
     
+    
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    notes.push({
+        title:'',
+        body:''
+    })
+    localStorage.setItem('notes',JSON.stringify(notes))
+    renderNotes(notes,filters)
+})
+    
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
     renderNotes(notes, filters)
+})
     
-    document.querySelector('#create-note').addEventListener('click', function (e) {
-        e.target.textContent = 'The button was clicked'
-    })
-    
-    document.querySelector('#search-text').addEventListener('input', function (e) {
-        filters.searchText = e.target.value
-        renderNotes(notes, filters)
-    })
-    
-    document.querySelector('#filter-by').addEventListener('change', function (e) {
-        console.log(e.target.value)
-    })
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
