4a5fd7cbfdcc108171492f5750e09e9c056956a5
Karim <karim@example.com>
2021-11-14 21:15:19 +0200

    Update

--- Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch8- Data Storage, Libraries, and More/notes-app(edit-note-version) 72/notes-edit.js
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch8- Data Storage, Libraries, and More/notes-app(edit-note-version) 72/notes-edit.js
@@ -1,3 +1,14 @@
+/**
+ * 1. Setup input event for title
+ * 2. Update note object and save note list
+ * 3. Repeat steps 1-2 for body
+ * 4. Setup a remove button that remotes notes and sends user back to home page
+ * 
+ */
+const titleEl = document.querySelector('#note-title')
+const bodyEl = document.querySelector('#note-body')
+const removeEl = document.querySelector('#remove-note')
+
 const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
 const note = notes.find(function(note){
@@ -8,5 +19,21 @@ if(note === undefined){
     location.assign('index.html')
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+titleEl.value = note.title
+bodyEl.value = note.body
+
+titleEl.addEventListener('input',function(e){
+    note.title = e.target.value
+    setNotes(notes)
+})
+
+bodyEl.addEventListener('input',function(e){
+    note.body = e.target.value
+    setNotes(notes)
+})
+
+removeEl.addEventListener('click',function(){
+    removeNote(note.id)
+    setNotes(notes)
+    location.assign('index.html')
+})
