b1a421bde91e01ea27e1cb350e495bf226519e8d
Karim <karim@example.com>
2021-11-17 01:40:26 +0200

    Update files

---
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch9- Expending Our javaScript Knowledge/notes-app(strict mode) 87/notes-function.js
@@ -0,0 +1,112 @@
+'use strict'
+// console.log(uuidv4())
+// Read exicting notes from localStorage
+const getSavedNotes = () => {
+    const notesJSON = localStorage.getItem('notes')
+    try{
+        return (notesJSON) ?  (JSON.parse(notesJSON)) : []
+    }catch(e){
+        return []
+    }
+}
+
+// Saved notes to storage
+const setNotes =(key,item) => {
+    localStorage.setItem('notes',JSON.stringify(notes))
+}
+
+// remove note after click at button
+const removeNote = (id) => {
+    const noteIndex = notes.findIndex((note) => {
+        return note.id === id
+    })
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+}
+
+// Generate the Dom strucure for a note
+const generateNoteDom = (note) => {
+    const noteEl = document.createElement('div')
+    const textEl = document.createElement('a')
+    const button = document.createElement('button')
+
+    // Setup the remove note button
+    button.textContent = 'x'
+    noteEl.appendChild(button)
+    button.addEventListener('click',function(){
+        // console.log(note)
+        removeNote(note.id)
+        setNotes(notes)
+        renderNotes(notes,filters)
+    })
+
+    // Setup the note title text
+    if(note.title.length > 0){
+        textEl.textContent = note.title
+
+    }else{
+        textEl.textContent = 'unnamed'
+    }
+    textEl.setAttribute('href',`edit-note.html#${note.id}`)
+
+    noteEl.appendChild(textEl)
+    return noteEl
+}
+
+// Sort your notes by one of three ways
+const sortNotes = (notes, sortBy) =>{
+    if (sortBy === 'byEdited') {
+        return notes.sort((a, b) => {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort((a, b) => {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
+// Render application notes
+const renderNotes = (notes, filters) => {
+    const filteredNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach((note) => {
+        const noteEl = generateNoteDom(note)
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+// Generete
+const generateLastEdited =  (timestamp) => {
+    return `Last edited ${moment(timestamp).fromNow()}`
+}
---
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch9- Expending Our javaScript Knowledge/todos-app (strict mode) 87/todos-app.js
@@ -0,0 +1,32 @@
+'use strict'
+let todos = getSavedTodos()
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodo(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input',  (e) => {
+    filters.searchText = e.target.value
+    // Render after take text to filter
+    renderTodo(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit',  (e) => {
+    e.preventDefault()
+    todos.push({
+        id:uuidv4(),
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    saveTodos(todos)
+    renderTodo(todos, filters)
+    e.target.elements.text.value = ''
+})
+
+document.querySelector('#hide-completed').addEventListener('change',  (e) => {
+    filters.hideCompleted = e.target.checked
+    renderTodo(todos, filters)
+})
