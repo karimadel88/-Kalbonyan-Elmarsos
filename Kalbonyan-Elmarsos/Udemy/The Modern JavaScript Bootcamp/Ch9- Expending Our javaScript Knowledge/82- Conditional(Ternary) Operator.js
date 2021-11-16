dfe7867264e96b84c55cd74ad839a74a698eb255
Karim <karim@example.com>
2021-11-16 22:15:47 +0200

    Conditional operator in javaScript

---
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch9- Expending Our javaScript Knowledge/82- Conditional(Ternary) Operator.js
@@ -0,0 +1,26 @@
+// const myAge = 27
+// let message 
+
+
+// If-conditional
+// if (myAge >= 19) {
+//     message = 'You can vote!'
+// }else{
+//     message = 'You cannot vote!'
+// }
+// console.log(message)
+
+// trenery-operator
+// message = ((myAge) >= 18) ? console.log('You can vote!') : console.log('You cannot vote!')
+
+// Third example 
+const myAge = 27
+const showPage = () => {
+    console.log('Showing the page')
+}
+const showErrorPage = ()=>{
+    console0log('Showing the error page')
+}
+
+myAge >= 21 ? showPage() : showErrorPage()
+
