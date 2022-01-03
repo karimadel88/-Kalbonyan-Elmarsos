756f67b416b7c6213719ad26a79b73955d9e08d4
Karim <karim@example.com>
2022-01-03 17:34:06 +0200

    Update

--- Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch11- Asynchronous JavaScript/104- Hangman/app.js
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch11- Asynchronous JavaScript/104- Hangman/app.js
@@ -1,28 +1,26 @@
-
-
 const puzzleEl = document.querySelector('#puzzle')
-const guessEl = document.querySelector('#gusses')
+const guessesEl = document.querySelector('#guesses')
+const game1 = new Hangman('Car Parts', 2)
 
-const g1 = new Hangman('cat parts',2)
-puzzleEl.textContent = g1.puzzel
-guessEl.textContent = g1.statusMessage
-
+puzzleEl.textContent = game1.puzzle
+guessesEl.textContent = game1.statusMessage
 
 window.addEventListener('keypress', function (e) {
     const guess = String.fromCharCode(e.charCode)
-    g1.makeGuess(guess)
-    puzzleEl.textContent = g1.puzzel
-    guessEl.textContent = g1.statusMessage
-   
+    game1.makeGuess(guess)
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
 })
 
-// Making request
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4){
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
+// Making http request
+const request = new XMLHttpRequest();
+request.open('GET','https://puzzle.mead.io/puzzle');
+request.send();
+
+request.addEventListener('readystatechange',(e)=>{
+    if(e.target.readyState === 4){
+        let data = JSON.parse(e.target.responseText)
+
+        console.log(data.puzzle);
     }
 })
-request.open('GET', 'https://puzzle.mead.io/puzzle')
-request.send()
--- Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch11- Asynchronous JavaScript/104- Hangman/hangman.js
+++ Kalbonyan-Elmarsos/Udemy/The Modern JavaScript Bootcamp/Ch11- Asynchronous JavaScript/104- Hangman/hangman.js
@@ -1,70 +1,59 @@
-// Http request
-// Request - What do we want to do
-// Responce - What was actully done
-
-class Hangman{
+class Hangman {
     constructor(word, remainingGuesses) {
         this.word = word.toLowerCase().split('')
         this.remainingGuesses = remainingGuesses
-        this.gussedLetters = []
+        this.guessedLetters = []
         this.status = 'playing'
     }
+    calculateStatus() {
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ')
 
-    // Methods
-    recalculatingStatus () {
-        // All letter in word must be in gussed
-        const finished = this.word.every((letter)=>{
-            return (this.gussedLetters.includes(letter) || letter === ' ')
-        })
-    
-        if (this.remainingGuesses === 0){
-            this.status = 'Failed'
-        }
-        else if(finished){
-            this.status = 'Finished'
-        }
-        else{
+        if (this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
+        } else {
             this.status = 'playing'
         }
     }
-
-    get statusMessage () {
-        if(this.status === 'playing'){
-            return `Gusses left: ${this.remainingGuesses}`
-        }else if (this.status === 'Failed') {
-            return `Nice try! the word was "${this.word.join('')}"`
-        }else{
-            return 'Well Done!'
+    get statusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.remainingGuesses}`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`
+        } else {
+            return 'Great work! You guessed the word.'
         }
     }
-
-    get puzzel (){
+    get puzzle() {
         let puzzle = ''
-        this.word.forEach(letter => {
-            if (this.gussedLetters.includes(letter) || letter ===' '){
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
                 puzzle += letter
-            }else{
+            } else {
                 puzzle += '*'
             }
-        });
+        })
+
         return puzzle
     }
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
 
-    makeGuess (guess){
-        const isUnique = this.gussedLetters.includes(guess)
-        const isBadGuess = this.word.includes(guess)
-    
-        if (this.status  !== 'playing'){
+        if (this.status !== 'playing') {
             return
         }
-    
-        if(!isUnique)
-            this.gussedLetters.push(guess)
-        
-        if(!isUnique && !isBadGuess)
-            this.remainingGuesses --
-    
-        this.recalculatingStatus()
+
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        }
+
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
+
+        this.calculateStatus()
     }
-        
 }
