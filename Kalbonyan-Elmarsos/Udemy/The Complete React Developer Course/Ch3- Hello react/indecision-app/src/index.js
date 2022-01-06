fd90d60c6e76e3d988cd252e89aaba94638b7558
Karim <karim@example.com>
2022-01-07 01:32:55 +0200

    Update

--- Kalbonyan-Elmarsos/Udemy/The Complete React Developer Course/Ch3- Hello react/indecision-app/src/index.js
+++ Kalbonyan-Elmarsos/Udemy/The Complete React Developer Course/Ch3- Hello react/indecision-app/src/index.js
@@ -1,74 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// // import App from './App';
-// import Counter from './Counter';
+import React from 'react'
+import ReactDom from 'react-dom';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-  
-//     {/*<App />*/}
-//     <Counter />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// Main data in my App
-const app = {
-  title : 'Indecision App',
-  subtitle : 'How to Be better in this life',
-  options : []
-};
-
-// Function to add task and prevent reload to form
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-    if(option){
-      app.options.push(option);
-      e.target.elements.option.value = ''
-    }
-    renderIndecisionApp();
+// Header have to text 
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision App</h1>
+        <h2>Make You Life Best</h2>
+      </div>
+    )
+  }
 }
 
-// Funtion To remove all tasks
-const removeAll = () => {
-  app.options = []
-  renderIndecisionApp();
+// Action to know what do
+
+class Action extends React.Component {
+  render(){
+    return(
+      <button>What I should do ?</button>
+    )
+  }
 }
 
-// Function to setup alert choose randomly task 
-const whatDo = () => {
-  const len = app.options.length;
-  const randomIndex = Math.floor((Math.random() * len));
-  alert(app.options[randomIndex]);
-  renderIndecisionApp();
-}
-
-// DOM to view details of page
-const appRoot = document.getElementById('root');
-
-// Main fumction to render state
-const renderIndecisionApp = () =>{ 
-  const template = (
-          <div>
-              <h1>{app.title}</h1>
-              {app.subtitle && <p>{app.subtitle}</p>}
-              {app.options.length > 0 ? <p>Here are your options</p> : "No options"}
-              <button disabled = {app.options.length===0} onClick={whatDo}>What I should do ?</button>
-              <button onClick={removeAll}>Remove All</button>
-              <ol>
-                {app.options.map((option)=><li key={option}>{option}</li>)}
-              </ol>
-              <form onSubmit={onFormSubmit}>
-                <input type='text' name='option' />
-                <button >A?dd Option</button>
-              </form>
-          </div>
+class Options extends React.Component {
+  render(){
+    return (
+      <div>Option here</div>
     );
-
-  // To render template1    
-  ReactDOM.render(template,appRoot);
+  }
 }
 
-renderIndecisionApp();
+class AddOption extends React.Component {
+  render(){
+    return(
+      <di>Add Option feild</di>
+    )
+  }
+}
+ 
+const template = (
+  <div>
+    <Header />
+    <Action />
+    <Options />
+    <AddOption />
+  </div>
+)
 
+ReactDom.render(template,document.getElementById('root'));
---
+++ Kalbonyan-Elmarsos/Udemy/The Complete React Developer Course/Ch3- Hello react/indecision-app/src/playGround/jsxIndecision.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// // import App from './App';
+// import Counter from './Counter';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+  
+//     {/*<App />*/}
+//     <Counter />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// Main data in my App
+const app = {
+  title : 'Indecision App',
+  subtitle : 'How to Be better in this life',
+  options : []
+};
+
+// Function to add task and prevent reload to form
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if(option){
+      app.options.push(option);
+      e.target.elements.option.value = ''
+    }
+    renderIndecisionApp();
+}
+
+// Funtion To remove all tasks
+const removeAll = () => {
+  app.options = []
+  renderIndecisionApp();
+}
+
+// Function to setup alert choose randomly task 
+const whatDo = () => {
+  const len = app.options.length;
+  const randomIndex = Math.floor((Math.random() * len));
+  alert(app.options[randomIndex]);
+  renderIndecisionApp();
+}
+
+// DOM to view details of page
+const appRoot = document.getElementById('root');
+
+// Main fumction to render state
+const renderIndecisionApp = () =>{ 
+  const template = (
+          <div>
+              <h1>{app.title}</h1>
+              {app.subtitle && <p>{app.subtitle}</p>}
+              {app.options.length > 0 ? <p>Here are your options</p> : "No options"}
+              <button disabled = {app.options.length===0} onClick={whatDo}>What I should do ?</button>
+              <button onClick={removeAll}>Remove All</button>
+              <ol>
+                {app.options.map((option)=><li key={option}>{option}</li>)}
+              </ol>
+              <form onSubmit={onFormSubmit}>
+                <input type='text' name='option' />
+                <button >A?dd Option</button>
+              </form>
+          </div>
+    );
+
+  // To render template1    
+  ReactDOM.render(template,appRoot);
+}
+
+renderIndecisionApp();
+
